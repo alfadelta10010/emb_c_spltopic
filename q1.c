84330813263cb1f43df6e14b7eeda73e905a86b5
Abhiram Dasika <alfadelta10010@gmail.com>
2023-09-19 14:23:58 +0530

    added question 1

---
+++ q1.c
@@ -0,0 +1,25 @@
+/*
+Date: 15th Sept, 2023
+Question: Write a program to print bigger value among the two variables. 
+Get input from user for the values and return the bigger one. 
+Print error message and stop if any of the numbers are negative
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	int a, b;
+	printf("Enter two values: ");
+	scanf("%d %d", &a, &b);
+	if((a > 0) && (b > 0))
+	{
+		if(a > b)
+			printf("%d > %d\n", a, b);
+		else
+			printf("%d < %d\n", a, b);
+	}
+	else
+		printf("error\n");
+	return 0;
+}
